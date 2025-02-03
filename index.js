3c5f7d2d99db9313d25a86d6c3f29b562447ddbe
Mohd Parvez Ansari <mdparvezansari91@gmail.com>
2025-02-03 17:03:44 +0400

    update

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const router = require("./routes/route");
 require("dotenv").config();
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 8000
 
+app.use(cors())
 app.use("/api",router);
 
 app.get("/", (req, res) => {
-    res.send("Hello world mondified");
+    res.json({
+        message: "Welcome to the API",
+    });
 });
 
 app.listen(PORT,()=>{
