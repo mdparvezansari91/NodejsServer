1739d096f3ec1ddf3604f61deb15a9927270aa12
Mohd Parvez Ansari <mdparvezansari91@gmail.com>
2025-02-03 16:43:45 +0400

    Update index.js

--- index.js
+++ index.js
@@ -3,18 +3,9 @@ require("dotenv").config();
 
 const app = express();
 
-const PORT = process.env.PORT || 8000
-
-
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("Hello world");
-})
+});
 
-
-app.listen(PORT,()=>{
-    console.log("server is listening")
-
-})
-
-
+// Export the app as a serverless function
 module.exports = app;
